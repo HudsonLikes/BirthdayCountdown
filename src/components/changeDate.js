a82cd3dd9c54e9e5e0f9f52d1736bfe99e1140a7
Jared Grant <jared.grant@canyonsdistrict.org>
2018-08-01 09:36:20 -0600

    g

---
+++ src/components/changeDate.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const ChangeDate= (title, callback) => {
+	return(
+		<button className="change-date" onClick={callback}>
+		<i className="fas fa-camera-retro"></i>
+		{title}
+
+
+		</button>
+
+
+
+		)
+
+}
+export default ChangeDate;
